a2cdf0ef6d045b4670de26b6cb1ce869a2a9f769
Dell <Dell@DESKTOP-8RTBFBC>
2023-07-31 10:59:22 +0530

    day one

---
+++ C2TC/src/com/cg/dayone/Identifierclass.java
@@ -0,0 +1,17 @@
+package com.cg.dayone;
+
+public class Identifierclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int maddy4=29;
+		int $mad=78;
+		int mad$=67;
+		int var_value=43;
+		System.out.println(a);
+		System.out.println(maddy4);
+
+	}
+
+}
