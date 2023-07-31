9bd7706266c453f52d6fa1663f86aa5bebebc404
Dell <Dell@DESKTOP-8RTBFBC>
2023-07-31 11:48:09 +0530

    daytow

--- C2TC/src/com/cg/daytow/ifelse.java
+++ C2TC/src/com/cg/daytow/ifelse.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class ifelse {
 
-			public static void main(String[] args) {
-				// TODO Auto-generated method stub
-				Scanner sc=new Scanner(System.in);
-				int score=sc.nextInt();
-				//ifelse
-				if(score>100) {
-					System.out.println("rain");
-				}
-				else {
-					System.out.println("no rain");
-				}
-				sc.close();
-			}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int score=sc.nextInt();
+		//ifelse
+		if(score>100) {
+			System.out.println("hot");
 		}
+		else {
+			System.out.println("cold");
+		}
+		sc.close();
+	}
+	}
